157a64531e36557cb209a922e305d623c313fe8f
yfji <jyf131462@163.com>
2017-04-16 16:45:08 +0800

    opencv camera

--- src/com/example/opencvcamera/ImageThread.java
+++ src/com/example/opencvcamera/ImageThread.java
@@ -26,6 +26,11 @@ public class ImageThread implements Runnable{
 	private Bitmap cacheImage;
 	private byte[] messageBuff;
 	private Handler h;
+	private int length=1024;
+	private int imageWidth;
+	private int imageHeight;
+	private int imageChannels;
+	private boolean safelyExit=false;
 	
 	public ImageThread(OutputStream os, InputStream _is, Handler _h){
 		ios=os;
@@ -35,7 +40,13 @@ public class ImageThread implements Runnable{
 	public void stop(){
 		bstop=true;
 	}
+	public boolean isStop(){
+		return safelyExit;
+	}
 	public void allocate(int w, int h, int n){
+		imageWidth=w;
+		imageHeight=w;
+		imageChannels=n;
 		buff = ByteBuffer.allocate(w*h*n);
 		cacheMat=new Mat(h, w, CvType.CV_8UC4);
 		cacheImage = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
@@ -50,11 +61,25 @@ public class ImageThread implements Runnable{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		safelyExit=false;
+		try {
+			String header="sz:"+imageWidth+","+imageHeight+","+imageChannels;
+			ios.write(header.getBytes());
+			while(true){
+				int rn=is.read(messageBuff);
+				String messageStr=new String(messageBuff, 0, rn);
+				Log.i("Socket", messageStr);
+				if(messageStr.equals("image"))
+					break;
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Log.i(TAG, "Image Thread running");	
 		while(!bstop){
-			Log.i(TAG, "Image Thread running");	
 			while(!newFrame){
 				try {
-					Thread.sleep(50);
+					Thread.sleep(10);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -62,15 +87,17 @@ public class ImageThread implements Runnable{
 			finishFrame=false;
 			Log.i("MatInfo", cacheMat.cols()+", "+cacheMat.rows()+", "+cacheMat.channels());
 			Utils.matToBitmap(cacheMat, cacheImage);
+			buff.rewind();
 			cacheImage.copyPixelsToBuffer(buff);
 	        byte[] byteArray = buff.array();
 	        try {
 				ios.write("image".getBytes());
+				ios.flush();
 				while(true){
 					int rn=is.read(messageBuff);
-					messageBuff[rn]='\0';
-					Log.i("Socket", new String(messageBuff));
-					if(new String(messageBuff).equals("image"))
+					String messageStr=new String(messageBuff, 0, rn);
+					Log.i("Socket", messageStr);
+					if(messageStr.equals("image"))
 						break;
 				}
 			} catch (IOException e1) {
@@ -78,7 +105,18 @@ public class ImageThread implements Runnable{
 			}
 	        //send;
         	try {
-				ios.write(byteArray, 0, byteArray.length);
+        		int start=0;
+        		int len=length;
+        		while(true){
+        			ios.write(byteArray, start, len);
+        			ios.flush();
+        			start+=len;
+        			if(start>=byteArray.length)
+        				break;
+        			if(start+len>=byteArray.length){
+        				len=byteArray.length-start;
+        			}
+        		}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -86,9 +124,9 @@ public class ImageThread implements Runnable{
         	while(true){
         		try {
 					int rn=is.read(messageBuff);
-					messageBuff[rn]='\0';
 	        		if(messageBuff[0]=='o' && messageBuff[1]=='k'){
-	        			String message=new String(messageBuff).substring(2);
+	        			String message=new String(messageBuff, 0, rn).substring(2);
+	        			Log.i("Socket", message);
 	        			Message msg=h.obtainMessage();
 	        			msg.what=1;
 	        			msg.obj=message;
@@ -98,7 +136,6 @@ public class ImageThread implements Runnable{
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-        		
         	}
         	try {
 				Thread.sleep(50);
@@ -110,20 +147,22 @@ public class ImageThread implements Runnable{
 		}
 		try {
 			ios.write("stop".getBytes());
+			safelyExit=true;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		/*
 		while(true){
     		try {
 				int rn=is.read(messageBuff);
-				messageBuff[rn]='\0';
-        		if(new String(messageBuff).equals("end")){
+				String messageStr=new String(messageBuff, 0, rn);
+        		if(messageStr.equals("end")){
+        			safelyExit=true;
         			break;
         		}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-    		
-    	}
+    	}*/
 	}
 }
